f3dc0ed994986828d43771c9fbc2f75245996038
Kason7 <kaspersonne27@gmail.com>
2021-06-22 11:28:01 +0100

    [Project] - adjusted tag manager id

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,26 @@
 // EXPORTED FUNCTIONS
-export const isEmpty = value =>
+export const isEmpty = (value) =>
   value === undefined ||
   value === null ||
   (typeof value === "object" && Object.keys(value).length === 0) ||
-  (typeof value === "string" && value.trim().length === 0)
+  (typeof value === "string" && value.trim().length === 0);
 
 export const imageRender = (image, size) => {
-  return image.split("/")[1].includes("avatars") ? `${process.env.REACT_APP_WEBKIT_URL}${image}/${size}` : image
-}
+  return image.split("/")[1].includes("avatars")
+    ? `${process.env.REACT_APP_WEBKIT_URL}${image}/${size}`
+    : image;
+};
 
 // TRACKING FUNCTIONS
-export const GA_TRACKING_ID = "GTM-12424"
-export const pageview = url => window.gtag("config", GA_TRACKING_ID, { pagePath: url, pageTitle: document.title })
+export const GA_TRACKING_ID = "GTM-KC6PD84";
+export const pageview = (url) =>
+  window.gtag("config", GA_TRACKING_ID, {
+    pagePath: url,
+    pageTitle: document.title,
+  });
 export const event = ({ action, category, label, value }) =>
-  window.gtag("event", action, { eventCategory: category, eventLabel: label, value: value })
+  window.gtag("event", action, {
+    eventCategory: category,
+    eventLabel: label,
+    value: value,
+  });
